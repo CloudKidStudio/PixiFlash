5e5b39977e4083f7d75e24658be286336b8a2809
Matt Karl <matt@mattkarl.com>
2015-08-31 21:59:11 -0400

    Auto-generated commit

---
+++ api.js
@@ -0,0 +1,27 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "pixiflash.Bitmap",
+        "pixiflash.Container",
+        "pixiflash.FlashArt",
+        "pixiflash.FlashArtTask",
+        "pixiflash.MovieClip",
+        "pixiflash.MovieClipPlugin",
+        "pixiflash.utils"
+    ],
+    "modules": [
+        "Pixi Flash",
+        "SpringRoll Plugin"
+    ],
+    "allModules": [
+        {
+            "displayName": "Pixi Flash",
+            "name": "Pixi Flash"
+        },
+        {
+            "displayName": "SpringRoll Plugin",
+            "name": "SpringRoll Plugin"
+        }
+    ]
+} };
+});
