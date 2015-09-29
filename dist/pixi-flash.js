876b623b2ab6505ed9aee2b901eb28d3043e6432
Matt Karl <matt@cloudkid.com>
2015-09-29 14:36:27 -0400

    Bumped version

--- dist/pixi-flash.js
+++ dist/pixi-flash.js
@@ -1,4 +1,4 @@
-/*! Pixi Flash 0.1.0 */
+/*! Pixi Flash 0.1.2 */
 /**
  * @module Pixi Flash
  * @namespace pixiflash
@@ -488,6 +488,36 @@
 		Timeline = createjs.Timeline,
 		Tween = createjs.Tween,
 		SharedTicker = PIXI.ticker.shared;
+	
+	//*** Note: the vast majority of the code here is from EaselJS's MovieClip class.
+	
+	/*
+	* MovieClip
+	* Visit http://createjs.com/ for documentation, updates and examples.
+	*
+	* Copyright (c) 2010 gskinner.com, inc.
+	*
+	* Permission is hereby granted, free of charge, to any person
+	* obtaining a copy of this software and associated documentation
+	* files (the "Software"), to deal in the Software without
+	* restriction, including without limitation the rights to use,
+	* copy, modify, merge, publish, distribute, sublicense, and/or sell
+	* copies of the Software, and to permit persons to whom the
+	* Software is furnished to do so, subject to the following
+	* conditions:
+	*
+	* The above copyright notice and this permission notice shall be
+	* included in all copies or substantial portions of the Software.
+	*
+	* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	* OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	* HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	* OTHER DEALINGS IN THE SOFTWARE.
+	*/
 	
 	/**
 	 * The class to emulate createjs.MovieClip, requires TweenJS
