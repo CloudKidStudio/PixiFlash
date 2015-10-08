01362f29f220a51ce3d6535e4e1a0097f92c8bd3
Eric <ericente@cloudkid.com>
2015-10-08 10:31:32 -0400

    release - Color Effects / ColorFilter

--- dist/pixi-flash.js
+++ dist/pixi-flash.js
@@ -1,4 +1,4 @@
-/*! Pixi Flash 0.2.3 */
+/*! Pixi Flash 0.2.4 */
 /**
  * @module Pixi Flash
  * @namespace pixiflash
--- dist/plugins/springroll.js
+++ dist/plugins/springroll.js
@@ -1,4 +1,4 @@
-/*! Pixi Flash 0.2.3 */
+/*! Pixi Flash 0.2.4 */
 /**
  * @module SpringRoll Plugin
  * @namespace pixiflash
