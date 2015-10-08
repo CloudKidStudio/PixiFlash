de71119bb9af0444ea6421c12929d39117b58899
Eric <ericente@cloudkid.com>
2015-10-08 00:06:43 -0400

    Auto-generated commit

---
+++ docs/api.js
@@ -0,0 +1,35 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "pixiflash.Bitmap",
+        "pixiflash.ColorFilter",
+        "pixiflash.Container",
+        "pixiflash.DisplayObject",
+        "pixiflash.FlashArt",
+        "pixiflash.FlashArtTask",
+        "pixiflash.Graphics",
+        "pixiflash.MovieClip",
+        "pixiflash.MovieClipInstance",
+        "pixiflash.MovieClipPlugin",
+        "pixiflash.Shape",
+        "pixiflash.Sprite",
+        "pixiflash.SpriteSheet",
+        "pixiflash.SpriteSheetTask",
+        "pixiflash.utils"
+    ],
+    "modules": [
+        "Pixi Flash",
+        "SpringRoll Plugin"
+    ],
+    "allModules": [
+        {
+            "displayName": "Pixi Flash",
+            "name": "Pixi Flash"
+        },
+        {
+            "displayName": "SpringRoll Plugin",
+            "name": "SpringRoll Plugin"
+        }
+    ]
+} };
+});
